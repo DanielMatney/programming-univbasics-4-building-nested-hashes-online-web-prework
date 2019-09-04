4937ba7b7a6fbbc4ef43cca76bfc1299e8d259b7
Daniel Matney <danieljmatney@gmail.com>
2019-09-04 02:06:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,7 @@ def monopoly_with_third_tier
     railroads:
     {
       pieces: 4,
-      rent_in_dollars: {:one_piece_owned => 25, rent2: 50, rent3: 100, rent4: 200},
+      rent_in_dollars: {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, four_pieces_owned => 200},
       :names => {name1: "a", name2: "b", name3: "c", name4: "d"}
     }
   }
